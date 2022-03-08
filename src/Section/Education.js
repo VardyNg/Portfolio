22ac65526a59f1f5d610b868c113d995bf77c5c8
Vardy <wadee@wadeenghoiwa.com>
2022-03-08 14:24:05 +0800

    deploy test

--- src/Section/Education.js
+++ src/Section/Education.js
@@ -57,7 +57,7 @@ function Education(){
     <>
       <div style={{alignContent: "left"}}>
         <Typography variant="h4" sx={{ textAlign: 'left' }} style={{fontFamily: "Raleway", padding: 10}}>
-          <b>Education Background</b>
+          <b>Education Background</b> (Test)
         </Typography>
       </div>
       <Stepper orientation="vertical"> 
