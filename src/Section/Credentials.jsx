e7fc6de87b6bbdeda9825a3d2a1d7dc8c5eef6c9
Vardy <wadee@wadeenghoiwa.com>
2022-07-16 16:39:08 +0800

    Certs display improvement

--- src/Section/Credentials.jsx
+++ src/Section/Credentials.jsx
@@ -2,6 +2,7 @@ import Typography from '@mui/material/Typography';
 import { ReactComponent as AWSSAA } from '../Images/Certs/AWS-SAA.svg';
 import { ReactComponent as AWSDVA } from '../Images/Certs/AWS-DVA.svg';
 import { ReactComponent as HTCAO002 } from '../Images/Certs/HTCAO002.svg';
+import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -17,14 +18,14 @@ function createDataForCerts(title, icon, issueDate, expirationDate, credentialLi
 
 const certs = [
   createDataForCerts(
-    "AWS Certified Solutions Architect – Associate",
+    "AWS Certified Solutions Architect – Associate (SAA-C02)",
     <AWSSAA style={{height: 100}}/>,
     "2022-04-03",
     "2025-04-03",
     "https://www.credly.com/badges/7794c6a8-5d2d-487d-992a-7beb697ce65e"
   ),
   createDataForCerts(
-    "AWS Certified Developer – Associate",
+    "AWS Certified Developer – Associate (DVA-C01)",
     <AWSDVA style={{height: 100}}/>,
     "2022-06-17",
     "2025-06-17",
@@ -41,34 +42,43 @@ const certs = [
 function Credentials(){
   
   return(
-    <div style={{marginTop: 10, marginBottom: 20}}>      
+    <div style={{marginTop: 10, marginBottom: 10}}>      
       <div style={{alignContent: "left"}}>
         <Typography variant="h4" sx={{ textAlign: 'left' }} style={{fontFamily: "Raleway", padding: 10}}>
           <b>Certifications</b>
         </Typography>
-        {certs.map(cert => (
-          <Card sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', padding: 3 }}>
-            {cert.icon}
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <CardContent >
-                <Typography component="div" variant="h6">
-                  {cert.title}
-                </Typography>
-                
-                <Typography variant="subtitle1" color="text.secondary" component="div">
-                  Validity: {moment(cert.issueDate).format("MMM YYYY")}
-                  {cert.expirationDate !== null && <> - {moment(cert.expirationDate).format("MMM YYYY")}</>}
-                </Typography>
-                <Button
-                  startIcon={<LinkIcon/>}
-                  onClick={() => window.open(cert.credentialLinkl)}
-                >
-                  Show credentials
-                </Button>
-              </CardContent>
-            </Box>
-          </Card>
-        ))}
+        <Grid 
+          container
+          style={{display: 'flex', flexDirection: 'row', flexWrap: 'stretch', justifyContent: 'space-around', alignItems: 'stretch'}}
+        >
+          {certs.map(cert => (
+            <Grid item xs={12} lg={6} padding={1}>
+              <Card sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', padding: 1 }}>
+                <div style={{marginLeft: 30}}>
+                  {cert.icon}
+                </div>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <CardContent >
+                    <Typography component="div" variant="h6">
+                      {cert.title}
+                    </Typography>
+                    
+                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                      Validity: {moment(cert.issueDate).format("MMM YYYY")}
+                      {cert.expirationDate !== null && <> - {moment(cert.expirationDate).format("MMM YYYY")}</>}
+                    </Typography>
+                    <Button
+                      startIcon={<LinkIcon/>}
+                      onClick={() => window.open(cert.credentialLinkl)}
+                    >
+                      Show credentials
+                    </Button>
+                  </CardContent>
+                </Box>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </div>
   )
