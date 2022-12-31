df6c1770194f2695c28d311fba39989c38496dd3
vardyng <vardy.ng@ecloudvalley.com>
2022-12-31 22:06:24 +0800

    add issuer

--- src/Section/Credentials.jsx
+++ src/Section/Credentials.jsx
@@ -21,15 +21,30 @@ import ReactGA from 'react-ga4';
 import { gaEvent } from "../functions";
 import { GA_CATEGORY_BUTTON_CLICK } from '../constants';
 
-function createDataForCerts(title, icon, issueDate, expirationDate, credentialLinkl, types, level){
-  return {title, icon, issueDate, expirationDate, credentialLinkl, types, level}
+function createDataForCerts(title, icon, issueDate, expirationDate, credentialLinkl, types, issuer){
+  return {title, icon, issueDate, expirationDate, credentialLinkl, types, issuer}
 }
 
 const tabs = [
-  { value: 'all', label: 'Overview' },
-  { value: 'csp', label: 'Cloud Service Provider' },
-  { value: 'iac', label: 'IaC' },
-  // { value: 'k8s', label: 'Kubernetes' },
+  { value: 'all', 
+    label: 'Overview',
+    issuers: [
+      "Amazon Web Service",
+      "HashiCorp"
+    ]
+  },
+  { value: 'csp', 
+    label: 'Cloud Service Provider',
+    issuers: [
+      "Amazon Web Service"
+    ]
+  },
+  { value: 'iac', 
+    label: 'IaC',
+    issuers: [
+      "HashiCorp"
+    ]
+  },
 ]
 
 const certs = [
@@ -39,7 +54,8 @@ const certs = [
     "2022-04-03",
     "2025-04-03",
     "https://www.credly.com/badges/7794c6a8-5d2d-487d-992a-7beb697ce65e",
-    ["all", "csp"]
+    ["all", "csp"],
+    "Amazon Web Service"
   ),
   createDataForCerts(
     "AWS Certified Developer – Associate (DVA-C01)",
@@ -47,7 +63,8 @@ const certs = [
     "2022-06-17",
     "2025-12-13",
     "https://www.credly.com/badges/610618a2-e827-48a0-9a9a-b80f7bc7cbcb",
-    ["all", "csp"]
+    ["all", "csp"],
+    "Amazon Web Service"
   ),
   createDataForCerts(
     "HashiCorp Certified: Terraform Associate (HTCA002)",
@@ -55,7 +72,8 @@ const certs = [
     "2022-07-16",
     "2024-07-16",
     "https://www.credly.com/badges/67e029c5-743c-4a93-9ef7-7fa78641d4a3",
-    ["all", "iac"]
+    ["all", "iac"],
+    "HashiCorp"
   ),
   createDataForCerts(
     "AWS Certified SysOps Administrator – Associate (SOA-C02)",
@@ -63,7 +81,8 @@ const certs = [
     "2022-10-14",
     "2025-12-13",
     "https://www.credly.com/badges/f929e2d3-42f6-473d-9e1a-766a0f8b8185",
-    ["all", "csp"]
+    ["all", "csp"],
+    "Amazon Web Service"
   ),
   createDataForCerts(
     "AWS Certified DevOps Engineer – Professional (DOP-C01)",
@@ -71,7 +90,8 @@ const certs = [
     "2022-12-13",
     "2025-12-13",
     "https://www.credly.com/badges/d808da6c-93f2-4fe3-84ec-e7932cb121ed/public_url",
-    ["all", "csp"]
+    ["all", "csp"],
+    "Amazon Web Service"
   ),
 ]
 
@@ -79,54 +99,73 @@ function CredentialsGroup(props){
   console.log(props)
   
   return (
-    <div hidden={props.value !== props.index}>
-      <Grid 
-          container
-          style={{
-            display: 'flex', 
-            flexDirection: 'row', 
-            flexWrap: 'stretch', 
-            justifyContent: 'space-around', 
-            alignItems: 'stretch', 
-            backgroundColor: ''
-          }}
-        >
-        {certs
-          .filter(cert => cert.types.includes(props.type))
-          .map(cert => (
-            <Grid item xs={12} lg={6} padding={1} key={cert} style={{backgroundColor: ''}}>
-              <Card sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', padding: 0.5, height: 125, backgroundColor: ''}}>
-                <div style={{marginLeft: 10}}>
-                  {cert.icon}
-                </div>
-                <Box sx={{ display: 'flex', flexDirection: 'column', backgroundColor: '', width: '100%', marginBottom: -2}}>
-                  <CardContent >
-                    <Typography component="div" variant="h7">
-                      {cert.title}
-                    </Typography>
-                    
-                    <Typography variant="subtitle1" color="text.secondary" component="div">
-                      Validity: {moment(cert.issueDate).format("MMM YYYY")}
-                      {cert.expirationDate !== null && <> - {moment(cert.expirationDate).format("MMM YYYY")}</>}
-                    </Typography>
-                    <Button
-                      startIcon={<LinkIcon/>}
-                      size="small"
-                      onClick={() => {
-                        gaEvent(GA_CATEGORY_BUTTON_CLICK, `Certification Link: ${cert.title}`);
-                        window.open(cert.credentialLinkl)
-                      }}
-                    >
-                      Show credentials
-                    </Button>
-                  </CardContent>
-                </Box>
-              </Card>
-            </Grid>
+    <div 
+      hidden={props.value !== props.index}
+      style={{
+        marginLeft: 10,
+        marginRight: 10
+      }}
+    >
+      {props.issuers
+        .map(issuer => {
+          return (
+            <> 
+              <Typography variant='overline'>
+                {issuer}
+              </Typography>
+              <hr style={{width: '30%', marginTop: -5}}/>
+              <Grid 
+                key={issuer}
+                container
+                style={{
+                  display: 'flex', 
+                  flexDirection: 'row', 
+                  flexWrap: 'stretch', 
+                  justifyContent: 'space-around', 
+                  alignItems: 'stretch', 
+                  backgroundColor: ''
+                }}
+              >
+                {certs
+                  .filter(cert => cert.issuer === issuer)
+                  .filter(cert => cert.types.includes(props.type))
+                  .map(cert => (
+                    <Grid item xs={12} lg={6} padding={1} key={cert} style={{backgroundColor: ''}}>
+                      <Card sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', padding: 0.5, height: 125, backgroundColor: ''}}>
+                        <div style={{marginLeft: 10}}>
+                          {cert.icon}
+                        </div>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', backgroundColor: '', width: '100%', marginBottom: -2}}>
+                          <CardContent >
+                            <Typography component="div" variant="h7">
+                              {cert.title}
+                            </Typography>
+                            
+                            <Typography variant="subtitle1" color="text.secondary" component="div">
+                              Validity: {moment(cert.issueDate).format("MMM YYYY")}
+                              {cert.expirationDate !== null && <> - {moment(cert.expirationDate).format("MMM YYYY")}</>}
+                            </Typography>
+                            <Button
+                              startIcon={<LinkIcon/>}
+                              size="small"
+                              onClick={() => {
+                                gaEvent(GA_CATEGORY_BUTTON_CLICK, `Certification Link: ${cert.title}`);
+                                window.open(cert.credentialLinkl)
+                              }}
+                            >
+                              Show credentials
+                            </Button>
+                          </CardContent>
+                        </Box>
+                      </Card>
+                    </Grid>
+                  )
+                )}
+              </Grid>
+            </>
           )
-        )}
-        
-      </Grid>
+        })
+      }
     </div>
     )
   }
@@ -156,7 +195,13 @@ function Credentials(){
         </Tabs>
         {tabs.map((tab, index) => {
           return (
-            <CredentialsGroup type={tab.value} value={tabValue} index={index} key={tab.value}/>
+            <CredentialsGroup 
+              type={tab.value} 
+              value={tabValue} 
+              index={index} 
+              key={tab.value}
+              issuers={tab.issuers}
+            />
           )
         })}
       </div>
