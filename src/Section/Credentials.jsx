b473e2dc5ae80d5f735e5d8c5a75c167fbe1811d
vardy.ng <vardy.ng@ecloudvalley.com>
2022-12-31 20:47:09 +0800

    Add tabs

--- src/Section/Credentials.jsx
+++ src/Section/Credentials.jsx
@@ -4,8 +4,10 @@ import { ReactComponent as AWSDVA } from '../Images/Certs/AWS-DVA.svg';
 import { ReactComponent as HTCAO002 } from '../Images/Certs/HTCAO002.svg';
 import { ReactComponent as AWSDOP } from '../Images/Certs/AWS-DOP.svg';
 import { ReactComponent as AWSSOA } from '../Images/Certs/AWS-SOA.svg';
-import Grid from '@mui/material/Grid'
-import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
@@ -17,9 +19,19 @@ import LinkIcon from '@mui/icons-material/Link';
 import ReactGA from 'react-ga4';
 import { gaEvent } from "../functions";
 import { GA_CATEGORY_BUTTON_CLICK } from '../constants';
-function createDataForCerts(title, icon, issueDate, expirationDate, credentialLinkl){
-  return {title, icon, issueDate, expirationDate, credentialLinkl}
+
+function createDataForCerts(title, icon, issueDate, expirationDate, credentialLinkl, types, level){
+  return {title, icon, issueDate, expirationDate, credentialLinkl, types, level}
 }
+
+const tabs = [
+  { value: 'all', label: 'Overview' },
+  // { value: 'aws', label: 'AWS' },
+  // { value: 'azure', label: 'Azure' },
+  { value: 'aws', label: 'Cloud Service' },
+  { value: 'azure', label: 'IaC' },
+  { value: 'azure', label: 'Kubernetes' },
+]
 
 const certs = [
   createDataForCerts(
@@ -27,21 +39,24 @@ const certs = [
     <AWSSAA style={{height: 100}}/>,
     "2022-04-03",
     "2025-04-03",
-    "https://www.credly.com/badges/7794c6a8-5d2d-487d-992a-7beb697ce65e"
+    "https://www.credly.com/badges/7794c6a8-5d2d-487d-992a-7beb697ce65e",
+    ["all", "aws"]
   ),
   createDataForCerts(
     "AWS Certified Developer – Associate (DVA-C01)",
     <AWSDVA style={{height: 100}}/>,
     "2022-06-17",
     "2025-12-13",
-    "https://www.credly.com/badges/610618a2-e827-48a0-9a9a-b80f7bc7cbcb"
+    "https://www.credly.com/badges/610618a2-e827-48a0-9a9a-b80f7bc7cbcb",
+    ["all", "aws"]
   ),
   createDataForCerts(
     "HashiCorp Certified: Terraform Associate (HTCA002)",
     <HTCAO002 style={{height: 100}}/>,
     "2022-07-16",
     "2024-07-16",
-    "https://www.credly.com/badges/67e029c5-743c-4a93-9ef7-7fa78641d4a3"
+    "https://www.credly.com/badges/67e029c5-743c-4a93-9ef7-7fa78641d4a3",
+    ["all", "aws"]
   ),
   createDataForCerts(
     "AWS Certified SysOps Administrator – Associate (SOA-C02)",
@@ -66,6 +81,17 @@ function Credentials(){
         <Typography variant="h4" sx={{ textAlign: 'left' }} style={{fontFamily: "Raleway", padding: 10}}>
           <b>Professional Qualifications</b>
         </Typography>
+        <Tabs 
+          // value={value} 
+          // onChange={handleChange} 
+          aria-label="basic tabs example"
+        >
+          {tabs.map((tab) => {
+            return (
+              <Tab label={tab.label} />
+            )
+          })}
+        </Tabs>
         <Grid 
           container
           style={{display: 'flex', flexDirection: 'row', flexWrap: 'stretch', justifyContent: 'space-around', alignItems: 'stretch'}}
