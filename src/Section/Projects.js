0496dbcb35d223621895cdb942f685e8dbdb9071
vardyng <vardy.ng@ecloudvalley.com>
2023-01-17 22:30:16 +0800

    resolve index same name issue

--- src/Section/Projects.js
+++ src/Section/Projects.js
@@ -116,9 +116,9 @@ function Projects(){
                     >
                       <div>
                         {
-                          Array.from(Array(project.start).keys()).map((item, import { NavLink } from 'react-router-dom') => (
+                          Array.from(Array(project.start).keys()).map((item, i) => (
                             <Rating
-                              key={import { NavLink } from 'react-router-dom'}
+                              key={i}
                               value={1}
                               max={1}
                             />
