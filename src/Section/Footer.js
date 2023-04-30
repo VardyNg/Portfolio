82af43eb89925286fcf16db8985fc83cd136483a
vardy.ng <wadee@wadeenghoiwa.com>
2023-04-30 14:58:27 +0800

    change year to current year

--- src/Section/Footer.js
+++ src/Section/Footer.js
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material"
 import { withStyles } from "@mui/styles";
-
+import moment from "moment";
 const WhiteTextTypography = withStyles({
   root: {
     color: "#FFFFFF"
@@ -11,7 +11,7 @@ function Footer(props){
   return(
     <div style={{padding: 10}}>
       <WhiteTextTypography variant="subtitle1" component="div" style={{fontColor: 'white'}}>
-        Created by Ng Hoi Wa, 2022. All right reserved.
+        Created by Ng Hoi Wa, 2021 - {moment().year()}. All right reserved.
       </WhiteTextTypography>
       <WhiteTextTypography variant="subtitle2" component="div" style={{fontColor: 'white'}}>
         written by ReactJS | hosted on AWS S3 static hosting | deployed using AWS CodePipeline | sourced on GitHub
