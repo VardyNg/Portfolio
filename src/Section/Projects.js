efb15f454e5a617b2729e10f56cc5619bbcdb1ba
vardy.ng <wadee@wadeenghoiwa.com>
2023-09-06 12:37:46 -0400

    add url and change title to clickable link if provided

--- src/Section/Projects.js
+++ src/Section/Projects.js
@@ -1,16 +1,17 @@
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Chip from '@mui/material/Chip';
 import Grid from '@mui/material/Grid';
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { useNavigate } from "react-router-dom";
+import LinkIcon from '@mui/icons-material/Link';
 
-function createDataForProjects(title, description, chip, link, start){
-  return {title, description, chip, link, start}
+function createDataForProjects(title, description, chip, link, start, url){
+  return {title, description, chip, link, start, url}
 }
 
 function Projects(){
@@ -18,18 +19,20 @@ function Projects(){
 
   const sideProjects = [
     createDataForProjects(
-      "Signal Sticker Maker",
+      "SignalStickerMaker.com",
       "A Web/Mobile app for making sticker for the Signal app, reached over 10,000 users world wide and created over 25,000 stickers",
       ["2 Developers", "Released"],
       "signal-sticker-maker",
-      3
+      3,
+      'https://signalstickermaker.com'
     ),
     createDataForProjects(
       "Private Tutor+",
       "A web application for private tutors to manage their business. The application is built with ReactJS, AWS Serverless (S3 + API Gateway + Lambda), Amplify, and MySQL.",
       ["4 Developers", "Archived"],
       "private-tutor-plus",
-      3
+      3,
+      null
     ),
   ]
 
@@ -40,6 +43,7 @@ function Projects(){
       <>{
         projects.map((project, index) => {
           console.log(project.chip.length)
+          const titleIsLink = project.url ? true : false
           return(
             <Card style={{margin: 20}} key={index}>
               <CardContent>
@@ -70,10 +74,25 @@ function Projects(){
                           textAlign: 'left', 
                           fontWeight: 'bold',
                           display: 'flex',
-                          justifyContent: 'center'
+                          justifyContent: 'center',
                         }}
                       >
-                        {project.title}
+                        {titleIsLink ? 
+                          <Link
+                            underline="hover"
+                            onClick={(e) => {
+                              window.open(project.url, '_blank')
+                            }}
+                            style={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              flexWrap: 'wrap',
+                            }}
+                          >
+                            {project.title}
+                            <LinkIcon />
+                          </Link>
+                        : project.title}
                       </Typography>
                     </Stack>
                     <Typography variant="body1" component="div" sx={{textAlign: 'left', marginTop: 1}}>
