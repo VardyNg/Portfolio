47b97457d8f664ed4d4a1676d001bc55e48fff99
vardy.ng <wadee@wadeenghoiwa.com>
2023-09-06 16:17:44 -0400

    update footer separater

--- src/Section/Footer.js
+++ src/Section/Footer.js
@@ -14,7 +14,7 @@ function Footer(props){
         Created by Vardy Ng, 2021 - {moment().year()}. All right reserved.
       </WhiteTextTypography>
       <WhiteTextTypography variant="subtitle2" component="div" style={{fontColor: 'white'}}>
-        written by ReactJS | hosted on AWS S3 static hosting | deployed using Jenkins | sourced on GitHub
+        written by ReactJS / hosted on AWS S3 static hosting / deployed using Jenkins / sourced on GitHub
       </WhiteTextTypography>
     </div>
   )
